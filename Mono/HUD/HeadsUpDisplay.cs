2b314c45392ae563d6e61df18366052115794f1e
StarryMic <alexander.r.elledge@gmail.com>
2024-06-09 17:16:41 -0400

    Added retry and win menus. Fixed bug preventing a full level load from working.

--- Mono/HUD/HeadsUpDisplay.cs
+++ Mono/HUD/HeadsUpDisplay.cs
@@ -12,7 +12,14 @@ public partial class HeadsUpDisplay : Control
 	// Internal Node References
 	private Label _speedLabel;
     private Label _timeLabel;
+
+    private Button _retryButton;
+    private Button _nextLevelButton;
 	
+    // Level trackers
+    [Export]private string CurrentLevel;
+    [Export]private string NextLevel;
+    
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -24,6 +31,20 @@ public partial class HeadsUpDisplay : Control
 		// Set internal references
 		_speedLabel = GetNode<Label>("SpeedLabel");
 		_timeLabel = GetNode<Label>("TimeLeftLabel");
+		_retryButton = GetNode<Button>("FailScreen/VBoxContainer/Button");
+		_nextLevelButton = GetNode<Button>("WinScreen/VBoxContainer/NextLevel");
+		
+		// Connect signals
+		_retryButton.Pressed += () => 
+		{
+			GetTree().Paused = false;
+			GetTree().ChangeSceneToFile(CurrentLevel);
+		};
+		_nextLevelButton.Pressed += () =>
+		{
+			GetTree().Paused = false;
+			GetTree().ChangeSceneToFile(NextLevel);
+		};
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,6 +64,6 @@ public partial class HeadsUpDisplay : Control
 
 	public void ShowWinScreen()
 	{
-		
+		this.GetNode<CenterContainer>("WinScreen").Visible = true;
 	}
 }
--- Mono/SecurityCam/SecurityCam.cs
+++ Mono/SecurityCam/SecurityCam.cs
@@ -8,12 +8,16 @@ public partial class SecurityCam : Sprite2D
 {
     private Area2D _detectionArea;
     private GameStateSignals _gameStateSignals;
+    private JuiceEffects _juice;
+
+    private bool hasSeen = false;
 
     public override void _Ready()
     {
         // Set node references
         _detectionArea = GetNode<Area2D>("Area2D");
         _gameStateSignals = GetNode<GameStateSignals>("/root/GameStateSignals");
+        _juice = GetTree().Root.GetChild(-1).GetNode<JuiceEffects>("JuiceEffects");
         
         // Connect event
         _detectionArea.BodyEntered += DetectionAreaOnBodyEntered;
@@ -21,15 +25,23 @@ public partial class SecurityCam : Sprite2D
 
     private void DetectionAreaOnBodyEntered(Node2D body)
     {
+        if (hasSeen)
+            return;
+        
         if (body is AstronautBasic)
         {
             _gameStateSignals.EmitSignal(GameStateSignals.SignalName.AlarmSetOff);
             GD.Print("I see an astronaut");
+            _juice.VignetteImpact();
+            _juice.BulletTime(1);
         }
         if (body is PlayerCharacter)
         {
             _gameStateSignals.EmitSignal(GameStateSignals.SignalName.AlarmSetOff);
             GD.Print("I see the player");
+            
+            _juice.VignetteImpact();
+            _juice.BulletTime(1);
         }
     }
 }
